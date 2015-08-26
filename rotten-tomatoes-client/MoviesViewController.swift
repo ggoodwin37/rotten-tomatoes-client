ce72badf4438f808a245607668d287e9c2321426
gideong <gideong@yahoo-inc.com>
2015-08-26 14:50:16 -0700

    WIP

--- rotten-tomatoes-client/MoviesViewController.swift
+++ rotten-tomatoes-client/MoviesViewController.swift
@@ -9,11 +9,23 @@
 import UIKit
 
 class MoviesViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")!
+        let request = NSURLRequest(URL:url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse?, data: NSData?, error: NSError?) -> Void in
+            do {
+                // TODO: I just want like no options here, wtf
+                let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments)
+                print(json)
+            } catch {
+                print("fuckin deserialization error")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
